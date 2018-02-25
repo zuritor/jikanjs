51b2af4722749cac6b17ca0a7dde29d5f95d6893
sven <sven2093@arcor.de>
2018-02-25 17:43:51 +0100

    refactoring and new Test cases

--- test/jikan.js
+++ test/jikan.js
@@ -10,13 +10,15 @@ describe(`${pkg.name}/Client`, function() {
 
     describe('#Anime', function() {
 
-        it('loaded anime should be Cowboy Bebop', function(done) {
+        it('loaded anime', function(done) {
             const response = jikanjs.loadAnime(1);
 
             response.then(function(response) {
                 assert.equal(response.mal_id, 1, 'wrong id, should be 1');
                 assert.equal(response.title, 'Cowboy Bebop', 'wrong title should be Cowboy Bebop');
                 done();
+            }).catch(function(error) {
+                assert.throws(error);
             })
         })
 
@@ -30,18 +32,74 @@ describe(`${pkg.name}/Client`, function() {
                 assert.equal(response.episode.length, 100, 'less or more than 100 episodes, should be 100');
                 assert.isAbove(response.episode_last_page, 7, 'not more than 7 pages, should be more');
                 done();
+            }).catch(function(error) {
+                assert.throws(error);
+            })
+        })
+
+        it('404 Not Found', function(done) {
+            const response = jikanjs.loadAnime(0);
+
+            response.then(function(response) {
+                assert.throws(response);
+            }).catch(function(error) {
+                assert.equal(error.status, 404);
+                done();
             })
         })
     })
 
     describe('#Manga', function() {
-        it('loaded manga should be Berserk', function(done) {
+        it('loaded manga', function(done) {
             const response = jikanjs.loadManga(2);
 
             response.then(function(response) {
                 assert.equal(response.mal_id, 2, 'wrong id, should be 2');
                 assert.equal(response.title, 'Berserk', 'wrong title should be Berserk');
                 done();
+            }).catch(function(error) {
+                assert.throws(error);
+            })
+        })
+    })
+
+    describe('#Person', function() {
+        it('loaded person', function(done) {
+            const response = jikanjs.loadPerson(8);
+
+            response.then(function(response) {
+                assert.equal(response.mal_id, 8, 'wrong id, should be 8');
+                assert.equal(response.name, 'Rie Kugimiya', 'wrong name should be Rie Kugimiya');
+                done();
+            }).catch(function(error) {
+                assert.throws(error);
+            })
+        })
+    })
+
+    describe('#Character', function() {
+        it('loaded character', function(done) {
+            const response = jikanjs.loadCharacter(43280);
+
+            response.then(function(response) {
+                assert.equal(response.mal_id, 43280, 'wrong id, should be 43280');
+                assert.equal(response.name, 'Inori Yuzuriha', 'wrong name should be Inori Yuzuriha');
+                done();
+            }).catch(function(error) {
+                assert.throws(error);
+            })
+        })
+    })
+
+    describe('#Search', function() {
+        it('search Anime', function(done) {
+            const response = jikanjs.search('anime', 'One Piece');
+
+            response.then(function(response) {
+                assert.isNotEmpty(response, 'response should not be empty');
+                done();
+            }).catch(function(error) {
+                assert.throws(error);
             })
         })
     })
