79aedef2dce7b26da8632ae536af044359ea30d4
sven <sven2093@arcor.de>
2018-03-02 15:24:10 +0100

    test suit upgraded to async / await

--- test/jikan.js
+++ test/jikan.js
@@ -8,7 +8,7 @@ const pkg                           = require('../package');
 
 describe(`${pkg.name}/Client`, function() {
 
-    this.timeout(4000);
+    this.timeout(5000);
 
     describe('#Anime', function() {
 
@@ -19,85 +19,84 @@ describe(`${pkg.name}/Client`, function() {
             assert.equal(response.title, 'Cowboy Bebop', 'wrong title should be Cowboy Bebop');
         })
 
-        it('episode request extension test',function(done) {
-            const response = jikanjs.loadAnime(21, 'episodes', 2);
+        it('episode request extension test', async function() {
+            const response = await jikanjs.loadAnime(21, 'episodes', 2);
             
-            response.then(function(response) {
-                assert.equal(response.mal_id, 21, 'wrong id, should be 21');
-                assert.equal(response.title, 'One Piece', 'wrong title should be One Piece');
-                assert.isNotEmpty(response.episode, 'is not empty');
-                assert.equal(response.episode.length, 100, 'less or more than 100 episodes, should be 100');
-                assert.isAbove(response.episode_last_page, 7, 'not more than 7 pages, should be more');
-                done();
-            }).catch(function(error) {
-                done(error);
-            })
-        })
-
-        it('404 Not Found', function(done) {
-            const response = jikanjs.loadAnime(0);
-
-            response.then(function(response) {
-                done(response);
-            }).catch(function(error) {
-                assert.equal(error.status, 404);
-                done();
-            })
+            assert.equal(response.mal_id, 21, 'wrong id, should be 21');
+            assert.equal(response.title, 'One Piece', 'wrong title should be One Piece');
+            assert.isNotEmpty(response.episode, 'is not empty');
+            assert.equal(response.episode.length, 100, 'less or more than 100 episodes, should be 100');
+            assert.isAbove(response.episode_last_page, 7, 'not more than 7 pages, should be more');
         })
     })
 
     describe('#Manga', function() {
-        it('loaded manga', function(done) {
-            const response = jikanjs.loadManga(2);
+        it('loaded manga',  async function() {
+            const response = await jikanjs.loadManga(2);
 
-            response.then(function(response) {
-                assert.equal(response.mal_id, 2, 'wrong id, should be 2');
-                assert.equal(response.title, 'Berserk', 'wrong title should be Berserk');
-                done();
-            }).catch(function(error) {
-                done(error);
-            })
+            assert.equal(response.mal_id, 2, 'wrong id, should be 2');
+            assert.equal(response.title, 'Berserk', 'wrong title should be Berserk');
         })
     })
 
     describe('#Person', function() {
-        it('loaded person', function(done) {
-            const response = jikanjs.loadPerson(8);
+        it('loaded person', async function() {
+            const response = await jikanjs.loadPerson(8);
 
-            response.then(function(response) {
-                assert.equal(response.mal_id, 8, 'wrong id, should be 8');
-                assert.equal(response.name, 'Rie Kugimiya', 'wrong name should be Rie Kugimiya');
-                done();
-            }).catch(function(error) {
-                done(error);
-            })
+            assert.equal(response.mal_id, 8, 'wrong id, should be 8');
+            assert.equal(response.name, 'Rie Kugimiya', 'wrong name should be Rie Kugimiya');
         })
     })
 
     describe('#Character', function() {
-        it('loaded character', function(done) {
-            const response = jikanjs.loadCharacter(43280);
+        it('loaded character', async function() {
+            const response = await jikanjs.loadCharacter(43280);
 
-            response.then(function(response) {
-                assert.equal(response.mal_id, 43280, 'wrong id, should be 43280');
-                assert.equal(response.name, 'Inori Yuzuriha', 'wrong name should be Inori Yuzuriha');
-                done();
-            }).catch(function(error) {
-                done(error);
-            })
+            assert.equal(response.mal_id, 43280, 'wrong id, should be 43280');
+            assert.equal(response.name, 'Inori Yuzuriha', 'wrong name should be Inori Yuzuriha');
         })
     })
 
     describe('#Search', function() {
-        it('search Anime', function(done) {
-            const response = jikanjs.search('anime', 'One Piece');
+        it('search Anime', async function() {
+            const response = await jikanjs.search('anime', 'One Piece');
 
-            response.then(function(response) {
-                assert.isNotEmpty(response, 'response should not be empty');
-                done();
-            }).catch(function(error) {
-                done(error);
-            })
+            assert.isNotEmpty(response, 'response should not be empty');
         })
+    })
+
+    describe('#Error Handling', function() {
+
+        it('400 - Invalid extended request', async function() {
+            var extension = 'titles';
+
+            try {
+                await jikanjs.loadAnime(21, extension);
+    
+            } catch (error) {
+                assert.equal(error.message, `Invalid extended request: "${extension}"`);
+                assert.equal(error.status, 400);
+            }
+        })    
+        
+        it('404 - No ID/Path Given', async function() {
+            try {
+                await jikanjs.loadAnime(0);
+    
+            } catch (error) {
+                assert.equal(error.message, 'No ID/Path Given');
+                assert.equal(error.status, 404);
+            }
+        })    
+
+        it('404 - File does not exist', async function() {
+            try {
+                await jikanjs.loadAnime(2);
+    
+            } catch (error) {
+                assert.equal(error.message, 'File does not exist');
+                assert.equal(error.status, 404);
+            }
+        })    
     })
 });
