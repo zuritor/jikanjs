6a11bcf1d07dfc046e56ddf3ed94adc5db6ac822
sven <sven2093@arcor.de>
2019-01-02 11:27:56 +0100

    updated to latest API version - #8 added new Route [Club, Club Members, Season Later, Season Archive] - improvements - #6 changed from axios to cross-fetch - readme update - travis-ci node version upgrade - jsdoc changes - added new Test - thanks to miraris for some inspiration

--- readme.md
+++ readme.md
@@ -12,19 +12,23 @@ Jikanjs is a small Wrapper for the unofficial MAL API [jikan.me](https://github.
 
 ## wrapped jikan Features
 
-* [Anime Parsing](https://github.com/zuritor/jikanjs#anime-parsing)
-* [Manga Parsing](https://github.com/zuritor/jikanjs#manga-parsing)
-* [Character Parsing](https://github.com/zuritor/jikanjs#character-parsing)
-* [People Parsing](https://github.com/zuritor/jikanjs#person-parsing)
-* [Search](https://github.com/zuritor/jikanjs#search)
-* [Seasonal Anime](https://github.com/zuritor/jikanjs#seasonal-anime)
-* [Anime Schedule](https://github.com/zuritor/jikanjs#anime-schedule)
-* [Genre](https://github.com/zuritor/jikanjs#genre) (v3)
-* [Producer](https://github.com/zuritor/jikanjs#producer) (v3)
-* [Magazine](https://github.com/zuritor/jikanjs#magazine) (v3)
-* [User](https://github.com/zuritor/jikanjs#user) (v3)
-* [Top](https://github.com/zuritor/jikanjs#top)
-* [Meta](https://github.com/zuritor/jikanjs#meta)
+* Anime Parsing
+* Manga Parsing
+* Character Parsing
+* People Parsing
+* Search
+* Seasonal Anime
+* Season Archive (v3)
+* Season Later (v3)
+* Anime Schedule
+* Genre (v3)
+* Producer (v3)
+* Magazine (v3)
+* User (v3)
+* Top
+* Club (v3)
+* Club Members (v3)
+* Meta
 
 ## Additional
 
@@ -37,273 +41,48 @@ const jikanjs  = require('jikanjs'); // Uses per default the API version 3
 ```
 
 ### Modify API Version
-To cange the API version the following can be done:
+It is possible to change the API version and the API Base URL
 
 ```javascript
 jikanjs.settings.version = 2; // changes the API version to 2
+jikanjs.settings.setBaseURL('apiurl'); // sets the API Base URL
+jikanjs.settings.setBaseURL('apiurl', 2); // sets also the api version
 ```
 
-### Anime Parsing
+### API Methods
+* All API functions are promised Based
+* Information of all possible parameter are located at the Jikan REST-ful API documentation https://jikan.docs.apiary.io/
 
 ```javascript
-// Retrieve anime Information from its id
-jikanjs.loadAnime(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// also it is possible to receive extra Information like characters_staff, episodes, etc..
-jikanjs.loadAnime(1, 'characters_staff').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// furthermore it is possible to get the secound, third, ... page of episodes if the anime has more than 100 episodes.
-jikanjs.loadAnime(1, 'episodes', 2).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/anime) for more Information.
-
-### Manga Parsing
-
-```javascript
-// Retrieve Manga Information from its id
-jikanjs.loadManga(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// for Manga's its also possible to receive extra Information like characters, news, etc..
-jikanjs.loadManga(1, 'characters_staff').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/manga) for more Information.
-
-### Character Parsing
-
-```javascript
-// Retrieve Character Information from its id
-jikanjs.loadCharacter(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// Its also possible to request pictures of the Character
-jikanjs.loadCharacter(1, 'pictures').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/character) for more Information.
-
-### Person Parsing
-
-```javascript
-// Retrieve Person Information from its id
-jikanjs.loadCharacter(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// Its also possible to request pictures of the Person
-jikanjs.loadCharacter(1, 'pictures').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/person) for more Information.
-
-### Search
-
-**A Search query musst have a length of minimum 3 letters**
-
-```javascript
-// execute a simple search query with an search query and the page number which should given back
-jikanjs.search(type, 'search query', 2).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// to execute a complexe search query with multiple parameter to filter the result
-// an object with the Advanced Search Parameters needs to be deliverd.
-var params = {
-    type: 'tv', // to receive only tv series
-    status: 'airing' // to receive only currently airing animes
-}
-jikanjs.search(type, 'search query', 2, params).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-For all Possible Advanced Search Parameters visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/search).
-
-### Seasonal Anime
-
-```javascript
-// Retrieve Information of a specific season by year and the season type
-jikanjs.loadSeason(2012, 'winter').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/season) for more Information.
-
-### Anime Schedule
-
-```javascript
-// Retrieve the current Schedule
-jikanjs.loadSchedule().then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// also it is possible to get the Schedule of a weekday
-jikanjs.loadSchedule('monday').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/schedule) for more Information.
-
-### Top
-
-```javascript
-// Retrieve Top List of e.g. Anime, Manga
-jikanjs.loadTop('anime').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// also its possible to specific define the page which should be returned
-jikanjs.loadTop('anime', 2).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// furthermore the top list can be filtered
-jikanjs.loadTop('anime', 2, 'tv').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-To see all possible subtypes which can be filtered visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/top).
-
-### Genre
-Added at jikan API version 3
-
-```javascript
-// Retrieve a List of all animes of specific Genre By its ID
-jikanjs.loadGenre('anime', 1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/genre) for more Information.
-
-### Producer
-Added at jikan API version 3
-
-```javascript
-// Retrieve Information of one specific Producer By its ID
-jikanjs.loadProducer(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/producer) for more Information.
-
-### Magazine
-Added at jikan API version 3
-
-```javascript
-// Retrieve Information of one specific Magazine By its ID
-jikanjs.loadMagazine(1).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-Visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/magazine) for more Information.
-
-### User
-Added at jikan API version 3
-
-```javascript
-// loads Information of a spezific MAL User by its username
-jikanjs.loadUser('zuritor').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-
-// addition load all animes in the History of a User
-jikanjs.loadUser('zuritor', 'history', 'anime').then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
-});
-```
-
-For more Options and Information what is possible visit [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/user).
-
-### Meta
-
-```javascript
-// get Meta information
-jikanjs.loadMeta(type, period)
-// for loading the status
+jikanjs.loadAnime(id [, request [, parameter]])
+jikanjs.loadManga(id [, request])
+jikanjs.loadPerson(id [, request])
+jikanjs.loadCharacter(id [, request])
+jikanjs.search(type, query [, page [, params]])
+jikanjs.loadSeason(year, season)
+jikanjs.loadSeasonArchive()
+jikanjs.loadSeasonLater()
+jikanjs.loadSchedule(day)
+jikanjs.loadTop(type [, page [, subtype]])
+jikanjs.loadGenre(type, id [, page])
+jikanjs.loadProducer(id [, page])
+jikanjs.loadMagazine(id [, page])
+jikanjs.loadUser(username [, request [, data]])
+jikanjs.loadClub(id)
+jikanjs.loadClubMembers(id [, page])
+jikanjs.loadMeta(type, period [, offset])
 jikanjs.loadStatus()
+jikanjs.raw(urlParts [, queryParameter])
 ```
-
-This is for request Information related to the Jikan REST Instance. See [jikan.me documentation](https://jikan.docs.apiary.io/#reference/0/meta) for more Information and all possible parameter arguments.
-
-## Raw
-
-The raw function can be used as an alternative way to use the Jiken.me API or for new API routes which are not yet covered by the current Jikanjs wrapper version
-
-NOTICE: can currently not be used for searches because of the latest API changes
+### Examples
+To print for example all episodes titles of the anime "No Game No Life" you can do the following:
 
 ```javascript
-// Parameter which should be used for the request.
-// The function will build the following url https://api.jikan.moe/anime/1
-var params = ['anime', 1];
-jikanjs.raw(params).then(function (response) {
-    // do stuff here
-}).catch(function (err) {
-    // handle error
+jikanjs.loadAnime(19815, 'episodes').then((response) => {
+    response.episodes.forEach(element => {
+        console.log(`${element.episode_id}: ${element.title} - ${element.title_romanji} - ${element.title_japanese}`);
+    })
+}).catch((err) => {
+    console.error(err); // in case a error happens
 });
 ```
--- test/jikan.js
+++ test/jikan.js
@@ -1,6 +1,3 @@
-//@ts-check
-'use strict';
-
 const assert                        = require('chai').assert;
 const jikanjs                       = require('../lib/jikan');
 const pkg                           = require('../package');
@@ -22,6 +19,17 @@ describe(`${pkg.name}/API/v2`, function() {
         done();
     });
 
+    describe('#General', function() {
+
+        it('API version', function() {
+            assert.equal(jikanjs.settings.version, 2, 'the api version should be 2');
+        });
+
+        it('API base url', function() {
+            assert.equal(jikanjs.settings.getBaseURL().href, 'https://api.jikan.moe/v2', 'the Base url should be https://api.jikan.moe/v2');
+        });
+    });
+
     describe('#Anime', function() {
 
         it('loaded anime', async function() {
@@ -29,7 +37,7 @@ describe(`${pkg.name}/API/v2`, function() {
 
             assert.equal(response.mal_id, 1, 'wrong id, should be 1');
             assert.equal(response.title, 'Cowboy Bebop', 'wrong title should be Cowboy Bebop');
-        })
+        });
 
         it('episode request extension test', async function() {
             const response = await jikanjs.loadAnime(21, 'episodes', 2);
@@ -39,8 +47,8 @@ describe(`${pkg.name}/API/v2`, function() {
             assert.isNotEmpty(response.episode, 'is not empty');
             assert.equal(response.episode.length, 100, 'less or more than 100 episodes, should be 100');
             assert.isAbove(response.episode_last_page, 7, 'not more than 7 pages, should be more');
-        })
-    })
+        });
+    });
 
     describe('#Manga', function() {
         it('loaded manga',  async function() {
@@ -83,7 +91,7 @@ describe(`${pkg.name}/API/v2`, function() {
                 await jikanjs.search('anime', query);
 
             } catch (error) {
-                assert.equal(error.message, `The given query must be of minium 3 letters! Given query '${query}' has only ${query.length} letters.`);
+                assert.equal(error.message, `The given query must be of minimum 3 letters! Given query '${query}' has only ${query.length} letters.`);
             }
         })
 
@@ -100,21 +108,39 @@ describe(`${pkg.name}/API/v2`, function() {
             const response = await jikanjs.loadSeason(2012, 'winter');
 
             assert.isDefined(response.season, 'season should be defined');
-            assert.equal(response.season_year, `2012`, 'Season Year shoulb be 2012');
+            assert.equal(response.season_year, `2012`, 'Season Year should be 2012');
             assert.equal(response.season_name, `Winter`, 'Season name should be Winter');
             assert.isNotEmpty(response, 'response should not be empty');
             assert.isAbove(response.season.length, 1, 'season should have minimum of one series');
         })
+
+        it('fetch all upcoming anime [Season Later] (Usable at API Version 3+)', async function() {
+            try {
+                await await jikanjs.loadSeasonLater();
+
+            } catch (error) {
+                assert.equal(error.message, `Usable at API version 3+`);
+            }
+        })
+
+        it('fetch all registered years with their seasons [Season Archive] (Usable at API Version 3+)', async function() {
+            try {
+                await await jikanjs.loadSeasonLater();
+
+            } catch (error) {
+                assert.equal(error.message, `Usable at API version 3+`);
+            }
+        })
     })
 
     describe('#Schedule', function() {
-        it('load the current shadule', async function() {
+        it('load the current schedule', async function() {
             const response = await jikanjs.loadSchedule();
 
             assert.isNotEmpty(response, 'response should not be empty');
         })
 
-        it('load shedule of monday', async function() {
+        it('load schedule of monday', async function() {
             const response = await jikanjs.loadSchedule('monday');
 
             assert.isDefined(response.monday, 'monday should be defined');
@@ -182,7 +208,7 @@ describe(`${pkg.name}/API/v2`, function() {
             assert.equal(response.top[0].rank, 51, 'wrong rank');
         })
 
-        it('load anime top page 1 and onyle for the subtype tv', async function() {
+        it('load anime top page 1 and only for the subtype tv', async function() {
             const response = await jikanjs.loadTop('anime', 1, 'tv');
 
             assert.isDefined(response.top, 'top should be defined');
@@ -192,8 +218,28 @@ describe(`${pkg.name}/API/v2`, function() {
         })
     })
 
+    describe('#Club', function() {
+        it('fetch information of the club with the id 1 (Usable at API Version 3+)', async function() {
+            try {
+                await await jikanjs.loadClub(1);
+
+            } catch (error) {
+                assert.equal(error.message, `Usable at API version 3+`);
+            }
+        })
+
+        it('fetch Member information of the club with the id 1 (Usable at API Version 3+)', async function() {
+            try {
+                await await jikanjs.loadClubMembers(1, 1);
+
+            } catch (error) {
+                assert.equal(error.message, `Usable at API version 3+`);
+            }
+        })
+    })
+
     describe('#Meta', function() {
-        it('load meta data of type anime in the perioade weekly', async function() {
+        it('load meta data of type anime in the periode weekly', async function() {
             const response = await jikanjs.loadMeta('anime', 'weekly');
 
             assert.isNotEmpty(response, 'response should not be empty');
@@ -217,20 +263,26 @@ describe(`${pkg.name}/API/v2`, function() {
             assert.equal(response.mal_id, 1, 'wrong id, should be 1');
             assert.equal(response.title, 'Cowboy Bebop', 'wrong title should be Cowboy Bebop');
         })
+
+        it('urlParts isn\'t an array', async function() {
+            var urlParts = {a: 'anime', b: 1};
+            try {
+                await await jikanjs.raw(urlParts);
+
+            } catch (error) {
+                assert.equal(error.message, `The given parameter should be an array like [anime, 1] but given was ${urlParts}`);
+            }
+        })
     })
-    /*
+
     describe('#Error Handling', function() {
 
         it('400 - Invalid extended request', async function() {
-            var extension = 'titles';
-
             try {
-                await jikanjs.loadAnime(21, extension);
+                await jikanjs.loadAnime(21, 'titles');
 
             } catch (error) {
-                assert.equal(error.message, `Invalid extended request: "${extension}"`);
-                assert.equal(error.status, 400);
-                assert.equal(error.url, `${jikanjs.settings.getBaseURL()}/anime/21/titles`);
+                assert.equal(error.message, `Invalid or incomplete endpoint`);
             }
         })
 
@@ -239,9 +291,7 @@ describe(`${pkg.name}/API/v2`, function() {
                 await jikanjs.loadAnime(0);
 
             } catch (error) {
-                assert.equal(error.message, 'No ID/Path Given');
-                assert.equal(error.status, 404);
-                assert.equal(error.url, `${jikanjs.settings.getBaseURL()}/anime`);
+                assert.equal(error.message, 'Invalid or incomplete endpoint');
             }
         })
 
@@ -250,13 +300,10 @@ describe(`${pkg.name}/API/v2`, function() {
                 await jikanjs.loadAnime(2);
 
             } catch (error) {
-                assert.equal(error.message, 'File does not exist');
-                assert.equal(error.status, 404);
-                assert.equal(error.url, `${jikanjs.settings.getBaseURL()}/anime/2`);
+                assert.equal(error.message, '404 on https://myanimelist.net/anime/2/');
             }
         })
     })
-    */
 });
 
 describe(`${pkg.name}/API/v3`, function() {
@@ -266,6 +313,17 @@ describe(`${pkg.name}/API/v3`, function() {
     beforeEach(done => {
         jikanjs.settings.version = 3;
         done();
+    });
+
+    describe('#General', function() {
+
+        it('API version', function() {
+            assert.equal(jikanjs.settings.version, 3, 'the api version should be 3');
+        });
+
+        it('API base url', function() {
+            assert.equal(jikanjs.settings.getBaseURL().href, 'https://api.jikan.moe/v3', 'the Base url should be https://api.jikan.moe/v3');
+        });
     });
 
     describe('#Anime', function() {
@@ -327,7 +385,7 @@ describe(`${pkg.name}/API/v3`, function() {
                 await jikanjs.search('anime', query);
 
             } catch (error) {
-                assert.equal(error.message, `The given query must be of minium 3 letters! Given query '${query}' has only ${query.length} letters.`);
+                assert.equal(error.message, `The given query must be of minimum 3 letters! Given query '${query}' has only ${query.length} letters.`);
             }
         })
 
@@ -352,21 +410,36 @@ describe(`${pkg.name}/API/v3`, function() {
             const response = await jikanjs.loadSeason(2012, 'winter');
 
             assert.isDefined(response.anime, 'season should be defined');
-            assert.equal(response.season_year, `2012`, 'Season Year shoulb be 2012');
+            assert.equal(response.season_year, `2012`, 'Season Year should be 2012');
             assert.equal(response.season_name, `Winter`, 'Season name should be Winter');
             assert.isNotEmpty(response, 'response should not be empty');
             assert.isAbove(response.anime.length, 1, 'season should have minimum of one series');
         })
+
+        it('load all upcoming anime [Season Later]', async function() {
+            const response = await jikanjs.loadSeasonLater();
+
+            assert.isNotEmpty(response, 'response should not be empty');
+            assert.isDefined(response.anime, 'season should be defined');
+            assert.equal(response.season_name, `Later`, 'Season type should be Later');
+        })
+
+        it('fetch all registered years with their seasons [Season Archive]', async function() {
+            const response = await jikanjs.loadSeasonArchive();
+
+            assert.isNotEmpty(response, 'response should not be empty');
+            assert.isDefined(response.archive, 'season archive should be defined');
+        })
     })
 
     describe('#Schedule', function() {
-        it('load the current shadule', async function() {
+        it('load the current schedule', async function() {
             const response = await jikanjs.loadSchedule();
 
             assert.isNotEmpty(response, 'response should not be empty');
         })
 
-        it('load shedule of monday', async function() {
+        it('load schedule of monday', async function() {
             const response = await jikanjs.loadSchedule('monday');
 
             assert.isDefined(response.monday, 'monday should be defined');
@@ -375,11 +448,11 @@ describe(`${pkg.name}/API/v3`, function() {
     })
 
     describe('#Genre', function() {
-        it('load Animes of the genre Action Anime', async function() {
+        it('load Anime of the genre Action Anime', async function() {
             const response = await jikanjs.loadGenre('anime', 1);
 
             assert.isNotEmpty(response, 'response should not be empty');
-            assert.equal(response.mal_url.mal_id, 1, 'returend genre should be 1');
+            assert.equal(response.mal_url.mal_id, 1, 'returned genre should be 1');
             assert.isDefined(response.anime, 'anime should be defined');
         })
     })
@@ -389,7 +462,7 @@ describe(`${pkg.name}/API/v3`, function() {
             const response = await jikanjs.loadProducer(1);
 
             assert.isNotEmpty(response, 'response should not be empty');
-            assert.equal(response.meta.mal_id, 1, 'returend producer id should be 1');
+            assert.equal(response.meta.mal_id, 1, 'returned producer id should be 1');
             assert.isDefined(response.anime, 'anime should be defined');
         })
     })
@@ -399,7 +472,7 @@ describe(`${pkg.name}/API/v3`, function() {
             const response = await jikanjs.loadMagazine(1);
 
             assert.isNotEmpty(response, 'response should not be empty');
-            assert.equal(response.meta.mal_id, 1, 'returend magazine id should be 1');
+            assert.equal(response.meta.mal_id, 1, 'returned magazine id should be 1');
             assert.isDefined(response.manga, 'manga should be defined');
         })
     })
@@ -436,7 +509,7 @@ describe(`${pkg.name}/API/v3`, function() {
             assert.equal(response.top[0].rank, 51, 'wrong rank');
         })
 
-        it('load anime top page 1 and onyle for the subtype tv', async function() {
+        it('load anime top page 1 and only for the subtype tv', async function() {
             const response = await jikanjs.loadTop('anime', 1, 'tv');
 
             assert.isDefined(response.top, 'top should be defined');
@@ -446,8 +519,24 @@ describe(`${pkg.name}/API/v3`, function() {
         })
     })
 
+    describe('#Club', function() {
+        it('load information of the Club with the id 1', async function() {
+            const response = await jikanjs.loadClub(1);
+
+            assert.isNotEmpty(response, 'response should not be empty');
+            assert.equal(response.mal_id, 1, 'returned club id should be 1');
+        })
+
+        it('load Member information of the Club with the id 1', async function() {
+            const response = await jikanjs.loadClubMembers(1);
+
+            assert.isNotEmpty(response, 'response should not be empty');
+            assert.isDefined(response.members, 'members should be defined');
+        })
+    })
+
     describe('#Meta', function() {
-        it('load meta data of type anime in the perioade weekly', async function() {
+        it('load meta data of type anime in the periode weekly', async function() {
             const response = await jikanjs.loadMeta('anime', 'weekly');
 
             assert.isNotEmpty(response, 'response should not be empty');
@@ -470,6 +559,46 @@ describe(`${pkg.name}/API/v3`, function() {
             assert.isNotEmpty(response, 'response should not be empty');
             assert.equal(response.mal_id, 1, 'wrong id, should be 1');
             assert.equal(response.title, 'Cowboy Bebop', 'wrong title should be Cowboy Bebop');
+        })
+
+        it('urlParts isn\'t an array', async function() {
+            var urlParts = {a: 'anime', b: 1};
+            try {
+                await await jikanjs.raw(urlParts);
+
+            } catch (error) {
+                assert.equal(error.message, `The given parameter should be an array like [anime, 1] but given was ${urlParts}`);
+            }
+        })
+    })
+
+    describe('#Error Handling', function() {
+
+        it('400 - Invalid extended request', async function() {
+            try {
+                await jikanjs.loadAnime(21, 'titles');
+
+            } catch (error) {
+                assert.equal(error.message, `Invalid or incomplete endpoint`);
+            }
+        })
+
+        it('404 - No ID/Path Given', async function() {
+            try {
+                await jikanjs.loadAnime(0);
+
+            } catch (error) {
+                assert.equal(error.message, 'Invalid or incomplete endpoint');
+            }
+        })
+
+        it('404 - File does not exist', async function() {
+            try {
+                await jikanjs.loadAnime(2);
+
+            } catch (error) {
+                assert.equal(error.message, '404 on https://myanimelist.net/anime/2/');
+            }
         })
     })
 });
